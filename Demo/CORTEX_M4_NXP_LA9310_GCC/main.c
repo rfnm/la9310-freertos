3d398a76e6ac852b7f6705c4e24870252a5fec81
Davide Cavion <davide@rfnm.io>
2024-03-24 15:38:06 +0100

    Other changes supporting TDD switching

--- Demo/CORTEX_M4_NXP_LA9310_GCC/main.c
+++ Demo/CORTEX_M4_NXP_LA9310_GCC/main.c
@@ -249,7 +249,8 @@ void iGpioInitRFIC( void )
 {
     int iCnt = 0;
 
-    for( iCnt = 6; iCnt <= 12; iCnt++ )
+   // for( iCnt = 6; iCnt <= 12; iCnt++ )
+    for( iCnt = 6; iCnt < 12; iCnt++ )
     {
         vGpioSetPinMuxSingle( iCnt, SET_MUX_GPIO_MODE );
         iGpioInit( iCnt, output, false );
